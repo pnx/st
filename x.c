d63def01da1ae9a56ce86ba4b15a4aa23ac150e0
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-05-11 16:45:40 +0200

    Support configure of underline offset

--- x.c
+++ x.c
@@ -1545,7 +1545,7 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 
         /* Render underline and strikethrough. */
         if (base.mode & ATTR_UNDERLINE) {
-            XftDrawRect(xw.draw, fg, winx, winy + dc.font.ascent + 1,
+            XftDrawRect(xw.draw, fg, winx, winy + dc.font.ascent + underline_offset,
                         width, 1);
         }
 
