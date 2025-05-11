9a91859b71c875e0af5319520bf2c9e37983babc
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-05-11 11:58:03 +0200

    Scrollback patches:
    st-scrollback-ringbuffer-0.9.2.diff
    st-scrollback-mouse-0.9.2.diff

--- config.def.h
+++ config.def.h
@@ -227,6 +227,8 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ ShiftMask,            Button4, kscrollup,      {.i = 10} },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = 10} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -252,6 +254,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
